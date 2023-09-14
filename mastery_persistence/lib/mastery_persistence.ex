3655db79d770418d0cbfc2a5c7f7467a8a822d11
Saul Perez <saul.perez@bunsan.io>
2023-09-14 06:51:39 -0600

    🚀 Created mastery_persistence app

---
+++ mastery_persistence/lib/mastery_persistence.ex
@@ -0,0 +1,18 @@
+defmodule MasteryPersistence do
+  @moduledoc """
+  Documentation for `MasteryPersistence`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MasteryPersistence.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
