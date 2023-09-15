79ae6cfb8ab6760cf97b218d89e1b87d19ffe1b9
Saul Perez <saul.perez@bunsan.io>
2023-09-15 11:38:12 -0600

    🧪 Test mastery and mastery_presistence

--- mastery_persistence/test/mastery_persistence_test.exs
+++ mastery_persistence/test/mastery_persistence_test.exs
@@ -1,8 +1,54 @@
 defmodule MasteryPersistenceTest do
   use ExUnit.Case
-  doctest MasteryPersistence
+  alias MasteryPersistence.{Response, Repo}
 
-  test "greets the world" do
-    assert MasteryPersistence.hello() == :world
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+
+    response = %{
+      quiz_title: :simple_addition,
+      template_name: :single_digit_addition,
+      to: "3 + 4",
+      email: "student@example.com",
+      answer: "7",
+      correct: true,
+      timestamp: DateTime.utc_now()
+    }
+
+    {:ok, %{response: response}}
+  end
+
+  test "responses are recorded", %{response: response} do
+    assert Repo.aggregate(Response, :count, :id) == 0
+    assert :ok = MasteryPersistence.record_response(response)
+    assert Repo.all(Response)
+           |> Enum.map(fn r -> r.email end) == [response.email]
+  end
+
+  test "a function can be run in the saving transaction", %{response: response} do
+    assert response.asnwer ==
+           MasteryPersistence.record_response(response, fn r -> r.answer end)
+  end
+
+  test "an error in the fucntion roll back the saeve", %{response: response} do
+    assert Repo.aggregate(Response, :count, :id) == 0
+    assert_raise RuntimeError, fn  ->
+      MasteryPersistence.record_response(response, fn _r -> raise "oops" end)
+    end
+    assert Repo.aggregate(Response, :count, :id)
+  end
+
+  test "simple reporting", %{response: response} do
+    MasteryPersistence.record_response(response)
+    MasteryPersistence.record_response(response)
+
+    response
+    |> Map.put(:email, "other_#{response.email}")
+    |> MasteryPersistence.record_response
+
+    assert MasteryPersistence.report(response.quiz_title) == %{
+      response.email => 2,
+      "other_#{response.email}" => 1
+    }
   end
 end
